ce84e5e8476613c1da250eaa97d9628f599c56b1
Jake Sanders <jsand@google.com>
2016-09-02 16:08:07 -0700

    Increment to v1.1.0
    Change-Id: I1adb9872c5c8ed77e2ff6f87b912ccbbcdf0c42a
    Signed-off-by: Jake Sanders <jsand@google.com>

--- config/config.go
+++ config/config.go
@@ -39,9 +39,9 @@ const (
 	// MajorVersion is the credential helper's major version number.
 	MajorVersion = 1
 	// MinorVersion is the credential helper's minor version number.
-	MinorVersion = 0
+	MinorVersion = 1
 	// PatchVersion is the credential helper's patch version number.
-	PatchVersion = 1
+	PatchVersion = 0
 )
 
 // SupportedGCRRegistries maps registry URLs to a bool representing whether
